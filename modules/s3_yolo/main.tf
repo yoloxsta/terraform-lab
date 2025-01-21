f4fa155fc4bccf1de1789884796c988b7ce2cda2
Soe Tint Aung <soetintaunghz10@gmail.com>
2025-01-21 19:27:24 +0630

    add:s3acl

--- modules/s3_yolo/main.tf
+++ modules/s3_yolo/main.tf
@@ -4,3 +4,29 @@ resource "aws_s3_bucket" "this" {
   tags = var.tags
 }
 
+resource "aws_s3_bucket_ownership_controls" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = var.block_public_acls
+  block_public_policy     = var.block_public_policy
+  ignore_public_acls      = var.ignore_public_acls
+  restrict_public_buckets = var.restrict_public_buckets
+}
+
+resource "aws_s3_bucket_acl" "this" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.this,
+    aws_s3_bucket_public_access_block.this,
+  ]
+
+  bucket = aws_s3_bucket.this.id
+  acl    = var.acl
+}
--- s3.tf
+++ s3.tf
@@ -2,4 +2,10 @@ module "s3_bucket" {
   source      = "./modules/s3_yolo"
   bucket_name = var.bucket_name
   tags        = var.tags
+  acl         = "public-read"
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
